b6058a9c7e9b31ba6c7497d6bccf1652be42e13d
nokuno <nokuno@nokuno.jp>
2010-07-10 15:18:49 +0900

    refactoring

--- python/ml/hmm.py
+++ python/ml/hmm.py
@@ -1,5 +1,5 @@
 #!/bin/env python
-k = 2
+K = 2
 
 def emission(x, z, mu):
     return mu[z][x]
@@ -11,24 +11,24 @@ def forward(X, A, pi, mu):
         alpha.append(
             [emission(x, j, mu) *
             sum(A[i][j]*alpha[-1][i]
-            for i in range(k))
-            for j in range(k)])
+            for i in range(K))
+            for j in range(K)])
     return alpha
 
 def backward(X, A, mu):
-    beta = [[1] * k]
+    beta = [[1] * K]
     for x in X:
         beta.append(
             [sum(A[j][i]*beta[-1][j]*emission(X[i], i, mu)
-            for i in range(k))
-            for j in range(k)])
+            for i in range(K))
+            for j in range(K)])
     return beta
 
 def baum_welch(X, A, pi, mu):
     alpha = forward(X, A, pi, mu)
     beta = backward(X, A, mu)
     return [[alpha[i][j]*beta[i][j]
-        for j in range(k)]
+        for j in range(K)]
         for i in range(len(X))]
 
 def likelyhood(X, A, pi, mu):
@@ -40,7 +40,7 @@ def display(result):
         print i, ':', result[i]
 
 if __name__ == '__main__':
-    #reverse state always
+    #test data
     A = [[0, 1],[1, 0]]
     X = [0, 2, 0, 1, 0, 2]
     pi = [0.75, 0.25]
