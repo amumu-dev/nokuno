871d5dedef256653647dd5ae678e29e2a6624ccd
nokuno <nokuno@nokuno.jp>
2010-07-11 21:45:00 +0900

    modify comment

--- python/ml/hmm.py
+++ python/ml/hmm.py
@@ -7,10 +7,10 @@ def emission(x, z, mu):
 
 def forward(X, A, pi, mu):
     l = len(X)
-    # \alpha(z_1=k) = \pi_k * p(x_1|\phi_k) (13.37)
+    # (13.37)
     alpha = [[pi[k] * emission(X[0], k, mu) for k in range(K)]]
     for x in X[1:]:
-        # \alpha(z_n) = p(x_n|z_n) \sum_{z_{n-1}} \alpha(z_{n-1}) p(z_n|z_{n-1}) (13.36)
+        # (13.36)
         alpha.append(
             [emission(x, j, mu) *
             sum(A[i][j]*alpha[-1][i]
@@ -22,7 +22,7 @@ def backward(X, A, mu):
     l = len(X)
     beta = [[1] * K]    # \beta(z_N) = 1
     for x in reversed(X[1:]):
-        # \beta(z_n) = \sum_{z_{n+1}} \beta(z_{n+1}) p(x_{n+1}|z_{n+1}) p(z_{n+1}|z_n) (13.38)
+        # (13.38)
         beta.insert(0,
             [sum(beta[0][i] * emission(x, i, mu) * A[j][i]
             for i in range(K))
