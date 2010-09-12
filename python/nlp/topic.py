ab3128e996b2487718bae6ae0a3b8a7197484855
nokuno <nokuno@nokuno.jp>
2010-09-13 00:36:29 +0900

    debug header

--- python/nlp/topic.py
+++ python/nlp/topic.py
@@ -6,10 +6,14 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-t", dest="threshold", type="int", default=0)
 parser.add_option("-w", dest="words", default="政治,スポーツ,経済")
+parser.add_option("-f", dest="format", default="title")
 (o, args) = parser.parse_args()
 topic_words = o.words.split(",")
 
-print "タイトル\t" + "\t".join(topic_words)
+if o.format == "title":
+    print "タイトル\t" + "\t".join(topic_words)
+else:
+    print "\t".join(topic_words)
 
 title = ""
 bag_of_words = {}
@@ -23,7 +27,9 @@ for line in sys.stdin:
             total = sum([bag_of_words.setdefault(topic,0) for topic in topic_words])
             if total > o.threshold:
                 prob = [str(float(bag_of_words.setdefault(topic,0)) / float(total)) for topic in topic_words]
-                print title + "\t" + "\t".join(prob)
+                if o.format == "title":
+                    print title + "\t",
+                print "\t".join(prob)
                     
         title = "".join(words[1:-1])
         bag_of_words = {}
