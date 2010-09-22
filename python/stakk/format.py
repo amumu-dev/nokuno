a3ecce509e49bc44149e1887941220f7b91653f3
nokuno <nokuno@nokuno.jp>
2010-09-22 17:20:01 +0900

    add format function

---
+++ python/stakk/format.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+#encoding: utf-8
+
+def format(x):
+    if type(x) == list:
+        result = "["
+        result += ', '.join(format(i) for i in x)
+        result += "]"
+    elif type(x) == tuple:
+        result = "("
+        result += ', '.join(format(i) for i in x)
+        result += ")"
+    elif type(x) == dict:
+        result = "{"
+        result += ', '.join(format(k)+": "+format(v) for k,v in x.items())
+        result += "}"
+    else:
+        result = str(x)
+    return result
+
+if __name__ == '__main__':
+    print format([1,2.0,"a"])
+    print format((1,2.0,"a"))
+    print format({1:1, 2.0:2.0, 'a':'a'})
+    print format({"よみ1":[("単語1_1",256),("単語2_2",32)]})
