2cff255f13641a763602eacf650769b82db3ec76
nokuno <nokuno@nokuno.jp>
2010-09-24 22:47:53 +0900

    add webapi of stakk

--- python/stakk/webapi.py
+++ python/stakk/webapi.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 #encoding: utf-8
+from urllib2 import unquote
+from pos_bigram import Converter
+
+
+dictionary="data/dictionary.txt"
+connection="data/connection.txt"
+
+converter = Converter(dictionary, connection, "\t")
 
 def application(environ, start_response):
     status = '200 OK'
-    #output = "Hello World!\n"
-    output = str(environ)
+    query = environ['QUERY_STRING'].split('=',1)[1]
+    query = unquote(query)
+    output = converter.convert(query)
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
@@ -14,3 +23,4 @@ def application(environ, start_response):
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     make_server('', 8080, application).serve_forever()
+
