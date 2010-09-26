df9e0ff4e07bba622d26799e4c705a883cb433c5
nokuno <nokuno@nokuno.jp>
2010-09-26 18:18:25 +0900

    implement sentence head and end

--- python/nlp/ngram.py
+++ python/nlp/ngram.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 #encoding: utf-8
-import sys
+from sys import stdin
 from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-n", dest="n", type="int", default=3)
 parser.add_option("-s", dest="separator", default="\t")
+parser.add_option("-o", dest="output", store_true)
 (options, args) = parser.parse_args()
 n = options.n
 
-for line in sys.stdin:
+for line in stdin:
     words = line.split(options.separator)
+    if options.output:
+        words.insert(0, "<S> <S> <S>")
+        words.append("</S> </S> </S>")
     for i in range(len(words)-n):
         print options.separator.join(words[i:i+n])
+
