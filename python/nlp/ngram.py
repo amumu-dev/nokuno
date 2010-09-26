267f53b530007bdc0a0fda2109545e57253d18a6
nokuno <nokuno@nokuno.jp>
2010-09-26 18:27:44 +0900

    debug

--- python/nlp/ngram.py
+++ python/nlp/ngram.py
@@ -6,15 +6,15 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-n", dest="n", type="int", default=3)
 parser.add_option("-s", dest="separator", default="\t")
-parser.add_option("-o", dest="output", store_true)
+parser.add_option("-o", dest="output", action="store_true")
 (options, args) = parser.parse_args()
 n = options.n
 
 for line in stdin:
-    words = line.split(options.separator)
-    if options.output:
-        words.insert(0, "<S> <S> <S>")
-        words.append("</S> </S> </S>")
-    for i in range(len(words)-n):
+    words = line.strip().split(options.separator)
+    if options.output and len(words) > 1:
+        words.insert(0, "<S>")
+        words.append("</S>")
+    for i in range(len(words)-n+1):
         print options.separator.join(words[i:i+n])
 
