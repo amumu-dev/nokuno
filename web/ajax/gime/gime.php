26fb964e4d67296817cbf5048d4e860e9c327a93
nokuno <nokuno@nokuno.jp>
2010-10-03 01:58:49 +0900

    update

--- web/ajax/gime/gime.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$base = "http://www.google.com/transliterate?langpair=ja-Hira|ja&text=";
-$query = $_GET['q'];
-$url = $base.$query;
-$result = file_get_contents($url);
-$object = json_decode($result, true);
-echo $object;
--- web/ajax/gime/gime.py
+++ web/ajax/gime/gime.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 #encoding: utf-8
-from urllib2 import urlopen
-from urllib import quote
-from re import sub
+from urllib import urlopen, quote
 
-def application(environ, start_response):
+def convert(query):
     base = "http://www.google.com/transliterate?langpair=ja-Hira|ja&text="
-    status = '200 OK'
-    query = environ['QUERY_STRING'].split('=',1)[1]
     url = base + query
     result = urlopen(url).read()
-    output = sub(r",\n]", r"\n]", result)
+    cands = result.split("\n")[5].split(",")
+    output = "\n".join([i[1:-1] for i in cands])
+    return output
 
+def application(environ, start_response):
+    status = '200 OK'
+    query = environ['QUERY_STRING'].split('=',1)[1]
+    output = convert(query)
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
     return [output]
 
+if __name__ == '__main__':
+    query = "わたし"
+    result = convert(query)
+    print result
