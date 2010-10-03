729596f74d7f0ed53a2c9dc14650737b35f6d542
nokuno <nokuno@nokuno.jp>
2010-10-03 11:15:31 +0900

    style sheet by summerwind

--- web/ajax/gime/index.php
+++ web/ajax/gime/index.php
@@ -2,6 +2,18 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Googleかな漢字変換APIのテスト</title>
+<style>
+.nokuno {
+    border-top: 1px solid #ccc;
+    border-left: 1px solid #ccc;
+}
+
+.nokuno td {
+    padding: 3px 8px;
+    border-bottom: 1px solid #ccc;
+    border-right: 1px solid #ccc;
+}
+</style>
 </head>
 <body>
 <h1>Googleかな漢字変換APIのテスト</h1>
@@ -18,7 +30,7 @@ if (isset($_GET['query'])) {
     $query = $_GET['query'];
     $result = `./fetch.py -q $query`;
     $lines = explode("\n", $result);
-    echo "<table border='1'>";
+    echo "<table class='nokuno' cellpadding='0' cellspacing='0'>";
     foreach($lines as $line) {
         if(strlen($line) == 0) continue;
         echo "<tr>";
@@ -26,7 +38,7 @@ if (isset($_GET['query'])) {
         foreach($items as $item) {
             echo "<td>$item</td>";
         }
-        echo "</tr>";
+        echo "</tr>\n";
     }
     echo "</table>";
 }
