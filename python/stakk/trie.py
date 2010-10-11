7a14c707cabb0c2bd4e58164fa0cf515af9ddb24
nokuno <nokuno@nokuno.jp>
2010-10-11 18:12:39 +0900

    distance info

--- python/stakk/trie.py
+++ python/stakk/trie.py
@@ -63,7 +63,7 @@ class Trie:
     def fuzzy_search(self, key, distance=1):
         results = []
         if not key and self.values:
-            results = [('', self.values)]
+            results = [('', distance, self.values)]
         if not self.children:
             return results
         if key:
@@ -71,13 +71,13 @@ class Trie:
             # match
             if first in self.children:
                 children = self.children[first].fuzzy_search(rest, distance)
-                results.extend((first + k,v) for k,v in children)
+                results.extend((first + k,d,v) for k,d,v in children)
             if distance > 0:
                 # replace
                 for k1, v1 in self.children.items():
                     if k1 != first:
                         children = v1.fuzzy_search(rest, distance-1)
-                        results.extend((k1+k,v) for k,v in children)
+                        results.extend((k1+k,d,v) for k,d,v in children)
                 # delete
                 children = self.fuzzy_search(rest, distance-1)
                 results.extend(children)
@@ -86,7 +86,7 @@ class Trie:
             # insert
             for k1, v1 in self.children.items():
                 children = v1.fuzzy_search(key, distance-1)
-                results.extend((k1+k,v) for k,v in children)
+                results.extend((k1+k,d,v) for k,d,v in children)
         return results
 
     def display(self, key="", depth=0):
