43fdbd5738fea35c90390b634565ef65c00c11b6
nokuno <nokuno@nokuno.jp>
2010-10-11 17:04:06 +0900

    implement fuzzy search with inser operation

--- python/stakk/trie.py
+++ python/stakk/trie.py
@@ -44,7 +44,7 @@ class Trie:
         else:
             if self.values:
                 results.append(('', self.values))
-            for k1,v1 in self.children.items():
+            for k1, v1 in self.children.items():
                 children = v1.predictive_search(key)
                 results.extend((k1+k,v) for k,v in children)
         return results
@@ -60,6 +60,27 @@ class Trie:
             result = self.children[first].search(rest)
         return result
         
+    def fuzzy_search(self, key, distance=1):
+        results = []
+        if not key and self.values:
+            results = [('', self.values)]
+        if not self.children:
+            return results
+        if not key and distance == 0:
+            return results
+        if key:
+            first, rest = key[0], key[1:]
+        if key and first in self.children:
+            children = self.children[first].fuzzy_search(rest, distance)
+            results.extend((first + k,v) for k,v in children)
+        elif distance > 0:
+            # insert
+            for k1, v1 in self.children.items():
+                children = v1.fuzzy_search(key, distance-1)
+                results.extend((k1+k,v) for k,v in children)
+                
+        return results
+
     def display(self, key="", depth=0):
         if self.values:
             print key + "\t" + format(self.values)
@@ -106,4 +127,8 @@ if __name__ == '__main__':
         print 'search: ', trie.search('try')
         print 'search: ', trie.search('tryed')
         print 'search: ', trie.search('trye')
+        print 'fuzzy: ', trie.fuzzy_search('try')
+        print 'fuzzy: ', trie.fuzzy_search('trye')
+        print 'fuzzy: ', trie.fuzzy_search('tay')
+        print 'fuzzy: ', trie.fuzzy_search('atry')
 
