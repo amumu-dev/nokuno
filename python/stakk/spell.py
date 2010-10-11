467824147523c632cf7a48481e6ae80cf00dded5
nokuno <nokuno@nokuno.jp>
2010-10-11 20:24:26 +0900

    update format

--- python/stakk/spell.py
+++ python/stakk/spell.py
@@ -16,7 +16,7 @@ class Entry:
         self.cost = cost
         self.rank = rank
     def __str__(self):
-        return format((self.word, self.yomi, self.distance, self.cost, self.rank))
+        return format((self.word, self.yomi, self.rank))
 
 #spelling corrector class
 class SpellingCorrector:
