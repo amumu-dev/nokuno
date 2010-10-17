e25b6d0628efe04783e0d0a04f9095f997495b20
nokuno <nokuno@nokuno.jp>
2010-10-17 16:53:35 +0900

    move dir

--- python/command/trans.py
+++ python/command/trans.py
@@ -3,6 +3,7 @@ from optparse import OptionParser
 from urllib import quote, urlopen
 from time import sleep
 from sys import exit
+from ConfigParser import SafeConfigParser
 
 parser = OptionParser()
 parser.add_option("-f", dest="filename")
@@ -16,16 +17,25 @@ parser.add_option("-w", dest="work", action="store_true")
 parser.add_option("-d", dest="debug", action="store_true")
 parser.add_option("-o", dest="open", action="store_true")
 parser.add_option("-r", dest="response", action="store_true")
+parser.add_option("-c", dest="config", default="~/.settings/trans.conf")
 (options, args) = parser.parse_args()
 
+if options.base:
+    base = options.base
+else:
+    config = SafeConfigParser()
+    config.read(options.config)
+    base = config.get("trans", "base")
+
 contents = open(options.filename).readlines()
+
 if len(contents) >= options.size:
     print "size over!", len(contents)
     exit()
 
 for i in range(0, len(contents), options.line):
     name = options.name + "%03d" % int(i/options.line)
-    u = options.base + "?file=" + name + "&text="
+    u = base + "?file=" + name + "&text="
     u += quote(''.join(contents[i:i+options.line]))
     if len(u) >= options.maximum:
         print "length over!", len(u), 'line', i
