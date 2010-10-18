ffd14e865a97b139dae89d544a19f12c2eb06367
nokuno <nokuno@nokuno.jp>
2010-10-18 23:31:37 +0900

    debug parse_json

--- python/twitter/parse_json.py
+++ python/twitter/parse_json.py
@@ -5,5 +5,6 @@ import sys, json
 for line in sys.stdin:
     if len(line) > 3:
         twit = json.loads(line)
-        print twit['user']['screen_name'], twit['text']
+        #print twit['user']['screen_name'], twit['text']
+        print twit['text'].encode('utf-8')
 
