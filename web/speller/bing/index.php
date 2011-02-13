9bfd8c536613ccb0df0ee64426773c9aae955627
nokuno <nokuno@nokuno.jp>
2011-02-13 20:38:04 +0900

    add

--- web/speller/bing/index.php
+++ web/speller/bing/index.php
@@ -7,7 +7,8 @@ if (isset($_GET['q'])) {
 } else {
     $query = "cofee";
 }
-$appid = "5061CD1C64F6015A128E51FE1FDFDC3B18D0610A";
+//$appid = "5061CD1C64F6015A128E51FE1FDFDC3B18D0610A";
+$appid = "F123BC971CAF06D542CA8AFE5AD589CA77F7F36A";
 $base = "http://api.search.live.net/xml.aspx";
 $ns = "http://schemas.microsoft.com/LiveSearch/2008/04/XML/spell";
 $url = "$base?Appid=$appid&sources=spell&query=$query";
@@ -15,12 +16,12 @@ $xml = simplexml_load_file($url);
 $xml->registerXPathNamespace('spl', $ns);
 //print_r($xml);
 $values = $xml->xpath('//spl:Value');
-$values[] = $query;
+//$values[] = $query;
 if (count($values) > 0) {
     $prob = 1.0 / count($values);
     foreach ($values as $value) {
         echo "$value\t$prob";
     }
 } else {
-    echo "$query\t1\n";
+    echo "$query\t1.0\n";
 }
