73906f8b2a1446d92eca2200b86e4c76034636ab
nokuno <nokuno@nokuno.jp>
2011-05-08 14:43:15 +0900

    add random_ab.py

---
+++ python/algorithm/random_ab.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import random
+
+def random_ab(a, b):
+    result = 0
+    for i in range(32):
+        result += (random.randint(0,1) << i)
+    return result % (b-a) + a
+
+if __name__ == '__main__':
+    print random_ab(1, 10)
