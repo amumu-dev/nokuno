574e145f28478f2d42b0573e3276cd1f04474e3e
nokuno <nokuno@nokuno.jp>
2011-07-04 14:33:15 +0900

    refactor ibm model

--- python/nlp/ibm.py
+++ python/nlp/ibm.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 from collections import defaultdict
+from optparse import OptionParser
 
-def train(corpus):
+def train(corpus, number):
     pair = defaultdict(float)
+    vocabulary = set()
     for english, forein in corpus:
         for e in english.split(" "):
             for f in forein.split(" "):
                 pair[(e,f)] += 1.
-    print 'pair:', pair
+                vocabulary.add(e)
 
     t = defaultdict(float)
     for e,f in pair.keys():    
-        t[(e,f)] = 0.25
-    for loop in range(1000):
+        t[(e,f)] = 1. / len(vocabulary)
+    for loop in range(number):
         count = defaultdict(float)
         total = defaultdict(float)
         s_total = defaultdict(float)
@@ -23,10 +25,29 @@ def train(corpus):
             total[f] += count[(e,f)]
         for e,f in pair.keys():    
             t[(e,f)] = count[(e,f)] / total[f]
-    print 'table:', t
+    return t
 
 if __name__ == '__main__':
-    corpus = [("the house", "das Haus"), 
-            ("the book", "das Buch"),
-            ("a book", "ein Buch")]
-    train(corpus)
+    parser = OptionParser()
+    parser.add_option("-e", dest="english", default="data/english.txt")
+    parser.add_option("-f", dest="foreign", default="data/foreign.txt")
+    parser.add_option("-t", dest="test", action="store_true")
+    parser.add_option("-n", dest="number", type="int", default=10)
+    parser.add_option("-p", dest="probability", type="float", default=0.01)
+    (options, args) = parser.parse_args()
+
+    if options.test:
+        corpus = [("the house", "das Haus"), 
+                ("the book", "das Buch"),
+                ("a book", "ein Buch")]
+    else:
+        english = [line.strip() for line in open(options.english)]
+        foreign = [line.strip() for line in open(options.foreign)]
+        corpus = zip(english, foreign)
+
+    t = train(corpus, options.number)
+
+    for (e,f),p in sorted(t.items(), key=lambda x:-x[1]):
+        if p >= options.probability:
+            print "%s\t%s\t%f" % (e, f, p)
+
