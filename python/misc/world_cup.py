ffd301ba86cf0a40670864602b342e733ad17b69
nokuno <nokuno@nokuno.jp>
2011-08-04 12:05:57 +0900

    add comment to world cup parser

--- python/misc/world_cup.py
+++ python/misc/world_cup.py
@@ -3,6 +3,19 @@ from sys import stdin
 from optparse import OptionParser
 from struct import unpack;
 
+# parse the structure
+# struct request
+# {
+#    uint32_t timestamp;
+#    uint32_t clientID;
+#    uint32_t objectID;
+#    uint32_t size;
+#    uint8_t method;
+#    uint8_t status;
+#    uint8_t type;
+#    uint8_t server;
+# };
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-f", dest="file", default="wc_day10_1.bin")
