7229c9fa108ef563162d8a04d506debfcd3c5adb
nokuno <nokuno@nokuno.jp>
2011-08-22 01:02:24 +0900

    update sample text

--- python/pyconjp/cabocha_test.py
+++ python/pyconjp/cabocha_test.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #encoding: utf-8
 from CaboCha import *
-
-# 入力する文字列
-text = "こんにちは世界！"
+text = "きっと何者にもなれないお前たちに告げる"
 parser = Parser("--charset=UTF8")
-
 print parser.parseToString(text)
+
+"""
 tree = parser.parse(text)
 print tree.toString(FORMAT_LATTICE)
 print tree.toString(FORMAT_XML)
+"""
