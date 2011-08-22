4fd58546f20afea76217360fedd878e6a8eb8b12
nokuno <nokuno@nokuno.jp>
2011-08-22 13:46:25 +0900

    refactoring

--- python/pyconjp/mecab_tokenizer.py
+++ python/pyconjp/mecab_tokenizer.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python
 #encoding: utf-8
+from nltk import *
+from nltk.text import *
 from nltk.corpus.reader import *
-from jptokenizer import *
-from nltk_jp import *
+from nltk.corpus.reader.util import *
+from nltk.tokenize.api import *
+from MeCab import *
 
-# 入力する文字列
-text = "こんにちは世界！"
+# NLTKをUTF-8に対応させる
+def init():
+    import sys
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
-reader = PlaintextCorpusReader("data/", "ginga.txt",
-        encoding="utf-8",
-        para_block_reader = read_line_block,
-        sent_tokenizer = jp_sent_tokenizer,
-        word_tokenizer = JPMeCabTokenizer())
+# Mecabによるトークナイザ
+class JPMeCabTokenizer(TokenizerI):
+    def __init__(self):
+        self.mecab = Tagger('-Owakati')
 
-print " ".join(reader.words()[20:80])
+    def tokenize(self, text):
+        result = self.mecab.parse(text.encode('utf-8'))
+        return result.decode('utf-8').strip().split(' ')
+
+# コーパスの読み込み
+def JapaneseCorpusReader(root, fieldids, encoding='utf-8'):
+    jp_sent_tokenizer = RegexpTokenizer(u'[^ 「」！？。]*[！？。]')
+    corpus = PlaintextCorpusReader(root, fieldids,
+            encoding = encoding,
+            para_block_reader = read_line_block,
+            sent_tokenizer = jp_sent_tokenizer,
+            word_tokenizer = JPMeCabTokenizer())
+
+    return corpus
+
+if __name__ == '__main__':
+    init()
+
+    # コーパスの読み込み
+    reader = JapaneseCorpusReader("data", "comike.txt")
+    text = Text(reader.words())
+
+    # 単語カウント
+    freq = FreqDist(text)
+    freq.tabulate(10)
+
+    # 共起語
+    text.collocations()
+
+    # 自動生成
+    text.generate()
+
+    # 検索
+    # text.concordance(u"は", 10, 10)
+
+    # 類似語
+    # text.similar(u"は")
 
